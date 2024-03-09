8d36f5149a62c002c61f85081939ddf2bab28619
ADARSH SINGH <109370113+Darkbucher@users.noreply.github.com>
2024-03-09 21:52:44 +0530

    Update script.js
    add video in redpill

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var videoPlayer = document.getElementById('redVideo');
     var nextButton = document.getElementById('nextButton');
 
-    var videoSources = ['red_video1.mp4', 'red_video2.mp4', 'red_video3.mp4', 'red_video4.mp4', 'red_video5.mp4', 'red_video6.mp4'];
+    var videoSources =  ['red_video1.mp4', 'redvideo7.mp4' ,'red_video2.mp4', 'redvideo8.mp4', 'red_video3.mp4', 'redvideo9.mp4','red_video4.mp4', 'redvideo10.mp4','red_video5.mp4', 'redvideo11.mp4', 'redvideo13.mp4','redvideo12.mp4','red_video6.mp4'];
     var currentVideoIndex = 0;
 
     // Set the first video source when the page loads
